fe8006b097703edb90e73db07db2f04a1bb5d461
Biran3170 <vanthanhthich@gmail.com>
2025-12-26 02:21:30 +0700

    Cap nhat hinh anh

--- src/stores/shop.js
+++ src/stores/shop.js
@@ -3,9 +3,9 @@ import { defineStore } from 'pinia'
 export const useShopStore = defineStore('shop', {
   state: () => ({
     products: [
-      { id: 1, name: 'Vòng cá đại dương', color: 'xanh nuoc bien, xanh, lam', price: 150000, stock: 5, img: 'path/to/img1.jpg' },
-      { id: 2, name: 'Vòng đỏ dâu tây', color: 'do', price: 50000, stock: 0, img: 'path/to/img2.jpg' },
-      { id: 3, name: 'Vòng bé mèo kẹo mút', color: 'trang', price: 300000, stock: 3, img: 'path/to/img3.jpg' }
+      { id: 1, name: 'Vòng cá đại dương', color: 'xanh nuoc bien, xanh, lam', price: 150000, stock: 5, img: '/img/cadaiduong.jpg' },
+      { id: 2, name: 'Vòng đỏ dâu tây', color: 'do', price: 50000, stock: 0, img: '/img/vongdodautay.jpg' },
+      { id: 3, name: 'Vòng bé mèo kẹo mút', color: 'trang', price: 300000, stock: 3, img: '/img/vongbemeokemut.jpg' }
     ],
     cart: []
   }),
